f92d99b0e17496794425d90a57a90087e984fefd
Chittoji Murali Sree Krishna <cmkrishna97@gmail.com>
2020-09-22 06:54:23 +0000

    Add files via upload

---
+++ routing/build/precache-manifest.43f484e1e56517abf94c93ed5d481efa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43de5376f06e4a43743c2278f161c81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0653501c8ff8f4cdd9ff",
+    "url": "/static/css/main.e5b80936.chunk.css"
+  },
+  {
+    "revision": "aefe1d8c00720fc3f806",
+    "url": "/static/js/2.46d8830a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.46d8830a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0653501c8ff8f4cdd9ff",
+    "url": "/static/js/main.d2403c0e.chunk.js"
+  },
+  {
+    "revision": "a8c346b69f56ee86bc0e",
+    "url": "/static/js/runtime-main.a9e8824e.js"
+  },
+  {
+    "revision": "8f01f51a610586a00ea69aa8640f5c25",
+    "url": "/static/media/mypic.8f01f51a.jpg"
+  }
+]);
